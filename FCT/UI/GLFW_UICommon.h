32d4fef3c117698a9acb4f8fb22e6f738f2e4f06
刘敏清 ch <2985861993@qq.com>
2025-03-22 15:07:29 +0800

    迁移到Windows(二)

--- FCT/UI/GLFW_UICommon.h
+++ FCT/UI/GLFW_UICommon.h
@@ -1,14 +1,26 @@
 //
 // Created by Administrator on 2025/3/22.
 //
-
+#include "../ThirdParty.h"
 #ifndef GLFW_UICOMMON_H
 #define GLFW_UICOMMON_H
 namespace FCT{
+
+    using UITaskFunction = std::function<void(void *)>;
+    struct UiTaskData {
+        UITaskFunction task;
+        void *param;
+        std::shared_ptr<bool> waiting;
+    };
+
 	class GLFW_UICommon {
 	public:
+		GLFW_UICommon();
+  		~GLFW_UICommon();
 
+        void postUiTask(UITaskFunction task,void* param = nullptr,bool waited = true);
     private:
+		std::thread m_uiThread;
 
 	};
 }
